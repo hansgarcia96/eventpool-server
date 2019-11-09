e487a34786219eef4947d28f0ed67f9446d632eb
Hans Garcia <hansgarcia96@gmail.com>
2019-11-09 16:13:33 -0500

    heroku server

---
+++ public/precache-manifest.7b71b6d617daf071098fff01ef5149d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c9f4642fa3f5bbce86cdf2c0858af45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22a0a2a19e21ebfa2b8a",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "667db6ed4b7180c7f87e",
+    "url": "/static/js/2.9a68b962.chunk.js"
+  },
+  {
+    "revision": "22a0a2a19e21ebfa2b8a",
+    "url": "/static/js/main.7acf031a.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  }
+]);
